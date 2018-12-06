c5e47ac26442cb521a2a3e30cc75ef2b25708fe7
steven7mwesigwa <ripper1355@gmail.com>
2018-12-06 09:11:09 +0300

    Added new file FunUtils.java

---
+++ src/java/com/luv2code/jsp/FunUtils.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luv2code.jsp;
+
+/**
+ *
+ * @author jaguar
+ */
+public class FunUtils {
+
+    public static String makeItLower(String data) {
+        return data.toLowerCase();
+    }
+}
