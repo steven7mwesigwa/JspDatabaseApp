9f87e21600ce7cda1b13efe57af9f51c66a6f014
steven7mwesigwa <ripper1355@gmail.com>
2018-12-20 02:45:21 +0300

    Added new file Student.java

---
+++ src/java/com/luv2code/jsp/tagdemo/Student.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luv2code.jsp.tagdemo;
+
+/**
+ *
+ * @author jaguar
+ */
+public class Student {
+
+    private String firstName;
+    private String lastName;
+//    goldCustomer is like a VIP customer that we may give special -
+//    priviledges to later on.
+    private boolean goldCustomer;
+
+    public Student(String firstName, String lastName, boolean goldCustomer) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.goldCustomer = goldCustomer;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isGoldCustomer() {
+        return goldCustomer;
+    }
+
+    public void setGoldCustomer(boolean goldCustomer) {
+        this.goldCustomer = goldCustomer;
+    }
+
+}
