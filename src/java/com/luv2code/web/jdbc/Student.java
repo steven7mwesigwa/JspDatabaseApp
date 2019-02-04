a5ac76d5563481f94a44969bec4185f2bb10461d
steven7mwesigwa <ripper1355@gmail.com>
2019-02-04 05:46:10 +0300

    Added javadoc documentation to the file.

--- src/java/com/luv2code/web/jdbc/Student.java
+++ src/java/com/luv2code/web/jdbc/Student.java
@@ -11,16 +11,29 @@ public class Student {
     private String lastName;
     private String email;
 
-//    A constructor version that doesn't use the "id" field.
-//    Because there a certain scenarios where by we don't have the "id" or 
-//    we don't need it for our student.
+    /**
+     * A constructor version that doesn't use the "id" field.
+     * Because there a certain scenarios where by we don't have the "id" or 
+     * we don't need it for our student.
+     * 
+     * @param firstName
+     * @param lastName
+     * @param email
+     */
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
-//    Second way of creating the "Student" class with an "id"
+    /**
+     * Second way of creating the "Student" class with an "id"
+     * 
+     * @param id
+     * @param firstName
+     * @param lastName
+     * @param email
+     */
     public Student(int id, String firstName, String lastName, String email) {
         this.id = id;
         this.firstName = firstName;
@@ -28,41 +41,77 @@ public class Student {
         this.email = email;
     }
 
+    /**
+     * Get student id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Set student id
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Get student's first name
+     * @return first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Set student's first name 
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Get student's last name 
+     * @return last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Set student's last name
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Get student's email 
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Set student's email 
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-//    Useful for debugging and logging information
-//    With this, we can easily get a dump of Student information without 
-//    having to manually do it ourselves.
+    /**
+     * Useful for debugging and logging information
+     * With this, we can easily get a dump of Student information without 
+     * having to manually do it ourselves.
+     * @return string representation of the "Student" object.
+     */
+    
     @Override
     public String toString() {
         return "Student{" + "id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + '}';
