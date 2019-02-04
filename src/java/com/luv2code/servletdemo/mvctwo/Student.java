fc75aec8fb77790e7b9d2ed8921872bc80016239
steven7mwesigwa <ripper1355@gmail.com>
2019-02-04 05:50:51 +0300

    Added javadoc documentation to the file.

--- src/java/com/luv2code/servletdemo/mvctwo/Student.java
+++ src/java/com/luv2code/servletdemo/mvctwo/Student.java
@@ -10,32 +10,62 @@ public class Student {
     private String lastName;
     private String email;
 
+    /**
+     * Constructor to create Student objects
+     * @param firstName
+     * @param lastName
+     * @param email
+     */
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
+    /**
+     * Get student's first name
+     * @return first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Set student's first name 
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Get student's last name 
+     * @return last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Set student's last name
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Get student's email 
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Set student's email 
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
